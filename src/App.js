31d025a6e85d2c8cd989b1f44cfba017a8b3f0f0
rahmatagungj <rahmatagungj@gmail.com>
2021-05-18 21:51:57 +0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+
 function App() {
   return (
     <div className="App">
